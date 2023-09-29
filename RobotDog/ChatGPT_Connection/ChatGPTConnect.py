784efecac70ac0201bffba24e1afb85385f22384
carme24036 <carme24036@live.ytech.edu>
2023-09-29 10:45:39 -0400

    small changes

--- RobotDog/ChatGPT_Connection/ChatGPTConnect.py
+++ RobotDog/ChatGPT_Connection/ChatGPTConnect.py
@@ -9,13 +9,6 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
-# Import the Go1 library
-# from droneblocks.go1_js import Go1, Go1Mode
-# dog = Go1()
-# dog.init()
-# dog.set_mode(Go1Mode.stand)
-# # dog.go_forward(0.25, 2000)
-
 # Function to extract code blocks from content
 import re
 def extract_code(content):
@@ -25,11 +18,14 @@ def extract_code(content):
     return code_blocks
 
 chat_prompt = """
-Hello ChatGPT
+Hello ChatGPT. You are going to be helping me control the Unitree Go1 Robot dog. 
+
+We use the code in the github repo at https://github.com/unitreerobotics/unitree_legged_sdk in the example_py folder to make the dog do things, such as walk, run, and change LED colors. 
+The IP Address of the dog is 172.16.3.197.
 """
 
 messages = [{"role": "system", "content": chat_prompt}]
-user_input = input("Welcome to ChatGPT. Feel free to ask questions.\n> ")
+user_input = input("Welcome to ChatGPT. Feel free to ask questions. To exit the program, type 'quit'.\n> ")
 
 bot_message = ""
 
@@ -49,7 +45,7 @@ while user_input != "quit":
 
             code_block = extract_code(bot_message['content'])
 
-            print("\n Bot-", bot_message['content'], "\n")
+            print("\nBot-", bot_message['content'])
 
             user_input = input("\n>")
         else:
